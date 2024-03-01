d8a773c5455e85640d806eb6e729a3dc0ff7df5b
Mikdhad.k <mikdhadk32@gmail.com>
2024-03-01 22:08:51 +0530

    search option

--- lib/features/fund/widgets/project_card.dart
+++ lib/features/fund/widgets/project_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stuverse/app/app.dart';
 import 'package:stuverse/features/fund/models/projects.dart';
+import 'package:stuverse/features/fund/routes/fund_routes.dart';
 
 class ProjectCard extends StatelessWidget {
   const ProjectCard({
@@ -13,64 +15,69 @@ class ProjectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: double.maxFinite,
-      margin: const EdgeInsets.only(bottom: 20),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surfaceVariant,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Hero(
-                tag: project.images,
-                child: Image.network(
-                  project.images,
-                  height: double.maxFinite,
-                  width: double.maxFinite,
-                  fit: BoxFit.fill,
+    return InkWell(
+      onTap: () {
+        context.push(FundRoutes.projectDesc);
+      },
+      child: Container(
+        height: 150,
+        width: double.maxFinite,
+        margin: const EdgeInsets.only(bottom: 20),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceVariant,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Hero(
+                  tag: project.images,
+                  child: Image.network(
+                    project.images,
+                    height: double.maxFinite,
+                    width: double.maxFinite,
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    project.title,
-                    style: context.titleLarge!
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    thickness: 1.5,
-                  ),
-                  Text(
-                    'Target Amount',
-                    textAlign: TextAlign.center,
-                    style: context.textTheme.bodyMedium!.copyWith(),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '\$ ${project.targetAmount}',
-                    textAlign: TextAlign.center,
-                    style: context.textTheme.bodyMedium!.copyWith(),
-                  ),
-                ],
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      project.title,
+                      style: context.titleLarge!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1.5,
+                    ),
+                    Text(
+                      'Target Amount',
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.bodyMedium!.copyWith(),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '\$ ${project.targetAmount}',
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.bodyMedium!.copyWith(),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
