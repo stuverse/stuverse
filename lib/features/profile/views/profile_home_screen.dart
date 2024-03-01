6755012646d59dc0cd5e225c08de439ed4013eda
Fathima Fida <fidafirozkattungal@gmail.com>
2024-03-01 20:55:55 +0530

    profile ui

--- lib/features/profile/views/profile_home_screen.dart
+++ lib/features/profile/views/profile_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stuverse/app/app.dart';
 
 class ProfileHomeScreen extends StatelessWidget {
@@ -14,52 +15,87 @@ class ProfileHomeScreen extends StatelessWidget {
         child: Column(
           children: [
             20.heightBox,
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: NetworkImage(user?.image ?? ""),
-            ),
-            20.heightBox,
-            Text(user?.name ?? "", style: context.headlineMedium),
-            6.heightBox,
-            Text(user?.email ?? "", style: context.titleMedium),
-            50.heightBox,
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white12,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 65,
+                  backgroundImage: NetworkImage(user?.image ?? ""),
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    ProfileTile(
-                      title: "Edit Profile",
-                      iconData: Icons.person,
-                      color: Theme.of(context).colorScheme.tertiaryContainer,
-                    ),
-                    ProfileTile(
-                        title: "My stats",
-                        iconData: Icons.circle_notifications_sharp,
-                        color:
-                            Theme.of(context).colorScheme.secondaryContainer),
-                    ProfileTile(
-                      title: "Settings",
-                      iconData: Icons.settings,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                    Divider(
-                      thickness: 6,
-                    ),
-                    10.heightBox,
-                    ProfileTile(
-                      title: "Logout",
-                      iconData: Icons.logout,
-                      color: Theme.of(context).colorScheme.error,
-                    )
-                  ],
+                Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.edit,
+                          size: 20,
+                        )))
+              ],
+            ),
+            10.heightBox,
+            Text(user?.name ?? "", style: context.headlineSmall),
+            Text(user?.email ?? "",
+                style: context.titleMedium!.copyWith(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onBackground
+                        .withOpacity(0.7))),
+            30.heightBox,
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white12,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      ProfileTile(
+                          title: "Edit Profile",
+                          iconData: Icons.person,
+                          color:
+                              Theme.of(context).colorScheme.tertiaryContainer,
+                          onTap: () {}),
+                      10.heightBox,
+                      ProfileTile(
+                          title: "My stats",
+                          iconData: Icons.circle_notifications_sharp,
+                          color:
+                              Theme.of(context).colorScheme.secondaryContainer,
+                          onTap: () {}),
+                      10.heightBox,
+                      ProfileTile(
+                          title: "Settings",
+                          iconData: Icons.settings,
+                          color: Theme.of(context).colorScheme.secondary,
+                          onTap: () {}),
+                      10.heightBox,
+                      Divider(
+                        thickness: 5,
+                      ),
+                      10.heightBox,
+                      ProfileTile(
+                        title: "Logout",
+                        iconData: Icons.logout,
+                        color: Theme.of(context).colorScheme.error,
+                        onTap: () {
+                          context.go(CommonRoutes.signin);
+                          context.read<CoreCubit>().signOut();
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             )
@@ -75,32 +111,37 @@ class ProfileTile extends StatelessWidget {
       {super.key,
       required this.title,
       required this.iconData,
-      required this.color});
+      required this.color,
+      required this.onTap});
   final String title;
   final Color color;
   final IconData iconData;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          margin: EdgeInsets.only(bottom: 10),
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            color: color.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(12),
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Icon(iconData, color: color),
           ),
-          child: Icon(iconData, color: color),
-        ),
-        20.widthBox,
-        Text(title, style: context.titleLarge),
-        Spacer(),
-        Icon(
-          Icons.chevron_right_rounded,
-        )
-      ],
+          20.widthBox,
+          Text(title, style: context.titleMedium),
+          Spacer(),
+          Icon(
+            Icons.chevron_right_rounded,
+          )
+        ],
+      ),
     );
   }
 }
