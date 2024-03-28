028b878707e67975e84ea839f35289e44d8a4acf
Faris Basha <fbashatm@gmail.com>
2024-03-29 00:09:03 +0530

    Add iOS ImageNotification files

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Faris Basha TM on 28/03/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
