6c4e186345cf616377b2473afca4017d519b2f66
Faris Basha <fbashatm@gmail.com>
2024-03-31 22:53:46 +0530

    Fix API endpoint and add thread summarization feature

--- lib/features/forum/services/api_endpoints.dart
+++ lib/features/forum/services/api_endpoints.dart
@@ -4,7 +4,8 @@ const TOGGLE_JOIN_COMMUNITY_API = '/forum/community/join/toggle/';
 const COMMUNITY_LIST_CREATE_API = '/forum/community/';
 const COMMUNITY_MANAGE_API = '/forum/community/<id>/';
 
-const GET_THREADS_API = '/forum//threads/';
+const GET_THREADS_API = '/forum/threads/';
+const THREAD_SUMMARIZE = '/forum/thread/<id>/summarize/';
 const MANAGE_THREAD_API = '/forum/thread/manage/';
 const VOTE_THREAD_API = '/forum/vote/thread/';
 const GET_THREAD_COMMENT_API = '/forum/thread/<id>/comments/';
